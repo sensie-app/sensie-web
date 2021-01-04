6e6111b5f6143e7b764c17d6930c54ed4630ed02
BraianVayletGlobant <braian.vaylet@globant.com>
2021-01-04 17:58:05 -0300

    working in changes->createAffirmations

--- src/dashboard/containers/CreateAffirmations/index.js
+++ src/dashboard/containers/CreateAffirmations/index.js
@@ -39,7 +39,8 @@ const CreateAffirmations = ({ initShowForm = true, withAffirmationsByTopics = tr
   const [title, setTitle] = useState(newAffirmation.title)
   const [topics, setTopics] = useState(newAffirmation.topics)
   const [listAffirmations, setListAffirmations] = useState([])
-  const [showErrorToast, setShowErrorToast] = useState(false)
+  const [showErrorTitle, setShowErrorTitle] = useState(false)
+  const [showErrorTopics, setShowErrorTopics] = useState(false)
   const [showNewForm, setShowNewForm] = useState(initShowForm)
 
   useEffect(() => dispatch(setNewAffirmationAction({ title, topics })), [title, topics])
@@ -71,10 +72,9 @@ const CreateAffirmations = ({ initShowForm = true, withAffirmationsByTopics = tr
    * @return {}
    */
   const handleClickBtnDone = () => {
-    if (title === '' || topics.length === 0) {
-      setShowErrorToast(true)
-    } else {
-      setShowErrorToast(false)
+    setShowErrorTitle(title === '')
+    setShowErrorTopics(topics.length === 0)
+    if (title !== '' && topics.length > 0) {
       inputRef.current.value = ''
       const _list = listAffirmations
       _list.push({ title, topics })
@@ -124,24 +124,27 @@ const CreateAffirmations = ({ initShowForm = true, withAffirmationsByTopics = tr
     return (
       <div className={styles.CreateAffirmationsForm}>
         <div className={styles.CreateAffirmationsFormTopContainer}>
-          <div className={`${styles.CreateAffirmationsFormD1} ${showErrorToast ? styles.CreateAffirmationsFormD1FlexError : styles.CreateAffirmationsFormD1Flex}`}>
+          <div className={`${styles.CreateAffirmationsFormD1} ${showErrorTitle || showErrorTopics ? styles.CreateAffirmationsFormD1FlexError : styles.CreateAffirmationsFormD1Flex}`}>
             <div className={styles.CreateAffirmationsFormD1Inputs}>
               <input
                 ref={inputRef}
                 placeholder={t('dashboard.CreateAffirmations.writeNewAffirmation')}
                 onChange={handleInputValue}
-                className={showErrorToast ? styles.inputBorderError : styles.inputBorder}
+                className={showErrorTitle ? styles.inputBorderError : styles.inputBorder}
               />
-              {showErrorToast && <span>{t('dashboard.CreateAffirmations.errorToast')}</span>}
+              {showErrorTitle && <span className={styles.errorMessage}>{t('dashboard.CreateAffirmations.errorTitle')}</span>}
             </div>
             <div className={styles.CreateAffirmationsFormD1Btns}>
-              <div className={showErrorToast ? styles.btnTopicsError : styles.btnTopics}>
-                <MultipleSelectCheckbox
-                  onClickValue={value => handleClickTopicMenu(value)}
-                  defValue={topics}
-                >
-                  {renderMultipleSelectCheckboxChildren()}
-                </MultipleSelectCheckbox>
+              <div>
+                <div className={showErrorTopics ? styles.btnTopicsError : styles.btnTopics}>
+                  <MultipleSelectCheckbox
+                    onClickValue={value => handleClickTopicMenu(value)}
+                    defValue={topics}
+                  >
+                    {renderMultipleSelectCheckboxChildren()}
+                  </MultipleSelectCheckbox>
+                </div>
+                {showErrorTopics && <span className={styles.errorMessage}>{t('dashboard.CreateAffirmations.errorTopic')}</span>}
               </div>
               <button onClick={() => handleClickBtnDone()} className={styles.CreateAffirmationsFormButtonDone}>Done</button>
             </div>
