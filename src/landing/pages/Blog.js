45b7c6e753b87abf40f03164e6fd1590cb1d185b
hans-strudle <strauslh@gmail.com>
2021-01-25 13:45:59 -0800

    testing iframe to blog

--- src/landing/pages/Blog.js
+++ src/landing/pages/Blog.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import Title from '../components/Title/Title'
-import { Box, makeStyles, Grid } from '@material-ui/core'
+// import Title from '../components/Title/Title'
+import IframeResizer from 'iframe-resizer-react'
+// import { Box, makeStyles, Grid } from '@material-ui/core'
 
-const useStyles = makeStyles({
-  background: {
-    height: '768px',
-    backgroundColor: '#071215'
-  }
-})
+// const useStyles = makeStyles({
+//   background: {
+//     height: '768px',
+//     backgroundColor: '#071215'
+//   }
+// })
 
 const Blog = () => {
-  const classes = useStyles()
+  // const classes = useStyles()
   return (
-        <Grid container className={classes.background}>
-          <Box mt={10}>
-            <Title title='BLOG INCOMING' titleDashboard />
-          </Box>
-        </Grid>
+    <IframeResizer
+      src="http://sensieapp.com/blog"
+      scrolling="yes"
+      style={{ width: '100%', minWidth: '100%', minHeight: '1000px', overflow: 'scroll' }}
+    />
   )
 }
 
