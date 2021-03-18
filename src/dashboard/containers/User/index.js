89285f998eb67534eb3134e38f5409a4ba1b1655
hans-strudle <strauslh@gmail.com>
2021-03-17 19:01:35 -0700

    default 0

--- src/dashboard/containers/User/index.js
+++ src/dashboard/containers/User/index.js
@@ -83,7 +83,7 @@ const User = ({ user, show, affirmation }) => {
       <div className={styles.UserBodySummary}>
         <div className={styles.UserBodyContainer}>
           <div><IconChart title={t('dashboard.IconChart.flow')} value={handleFlow(filterSensies(user.sensies.items))} valueType="%" icon={ACTIVITY} theme={2} /></div>
-          <PercentageChart title={t('dashboard.User.awarness')} value={user.selfAwareness} />
+          <PercentageChart title={t('dashboard.User.awarness')} value={user.selfAwareness || 0} />
           <div><IconChart title={t('dashboard.IconChart.engagement')} value={handleEngagement(globalDateFilter.value, handleTotalSensies())} icon={UP} theme={2} /></div>
           <div><IconChart title={t('dashboard.IconChart.sensies')} value={handleTotalSensies()} valueType="number" icon={UP} theme={2} /></div>
         </div>
