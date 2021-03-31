577f14ac1de78a615354c9734c569d3e7a717a56
hans-strudle <strauslh@gmail.com>
2021-03-30 20:43:35 -0700

    fix aff chart

--- src/dashboard/components/AffirmationChart/index.js
+++ src/dashboard/components/AffirmationChart/index.js
@@ -61,8 +61,8 @@ const AffirmationChart = ({ data, multiUser = true, value, onClickValue = () => 
           <span className={styles.AffirmationChartChartText}>{handleValue0('0%', handleValue())}</span>
         </div>
         <div style={{ width: '100%' }}>
-          <span className={styles.AffirmationChartText} style={{ float: 'left' }}>{ 'Pack: ' + data._packName }</span>
-          <span className={styles.AffirmationChartText} style={{ float: 'right' }}><i>{multiUser && (data._userCount + ' users') } ({data.sensies.items.length} Sensies)</i></span>
+          <span className={styles.AffirmationChartText} style={{ float: 'left' }}><i>{multiUser && (data._userCount + ' users') } ({data.sensies.items.length} Sensies)</i></span>
+          <span className={styles.AffirmationChartText} style={{ float: 'right' }}>{ 'Pack: ' + data._packName }</span>
         </div>
       </button>
     </section>
